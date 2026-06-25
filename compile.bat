e840fd765063ab42529f2636a6f5a9f44d9738e3
dual-core <131370751+dual-core1@users.noreply.github.com>
2026-06-25 05:51:54 -0500

    Add files via upload

---
+++ compile.bat
@@ -0,0 +1,2 @@
+:: clang compile for windows subsystem 
+clang++ main.cpp -mwindows -municode -ld2d1 -luser32 -lgdi32 -o D2DTEST.EXE
---
+++ main.cpp
@@ -0,0 +1,78 @@
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include <d2d1.h>
+
+LPCWSTR CLASS_NAME = L"Direct2D Window Class";
+
+// forward declaration of wndproc
+LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+int wWinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPWSTR lpCmdLine, int nCmdShow) {
+    // 1. create + register window class
+    WNDCLASSEX wc = {};
+    wc.cbSize = sizeof(WNDCLASSEX);
+    wc.lpfnWndProc = WndProc;
+    wc.hInstance = hInst;
+    wc.lpszClassName = CLASS_NAME;
+    wc.hCursor = LoadCursorW(NULL, IDC_ARROW);
+    RegisterClassExW(&wc);
+
+    // 2. create window
+    HWND hWnd = CreateWindow(CLASS_NAME, L"Direct2D Window", WS_OVERLAPPED,
+    CW_USEDEFAULT, CW_USEDEFAULT, 800, 600, NULL, NULL, hInst, NULL);
+    ShowWindow(hWnd, nCmdShow);
+    UpdateWindow(hWnd);
+
+    // 3. initialize direct 2d
+
+    // create factory
+    ID2D1Factory* pD2DFactory = NULL;
+    HRESULT hr = D2D1CreateFactory(D2D1_FACTORY_TYPE_SINGLE_THREADED, &pD2DFactory);
+    if (FAILED(hr))
+        return -1;
+
+    // create render target
+    RECT rc;
+    GetClientRect(hWnd, &rc);
+
+    ID2D1HwndRenderTarget* pRenderTarget = NULL;
+    hr = pD2DFactory->CreateHwndRenderTarget(
+        D2D1::RenderTargetProperties(),
+        D2D1::HwndRenderTargetProperties(
+            hWnd,
+            D2D1::SizeU(
+                rc.right - rc.left,
+                rc.bottom - rc.top
+            )
+        ),
+        &pRenderTarget
+    );
+    if (FAILED(hr))
+        return -1;
+
+    // 4. main loop + issue drawing commands
+    MSG msg = {};
+
+    while (msg.message != WM_QUIT) {
+        while (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE)) {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+
+        pRenderTarget->BeginDraw();
+        pRenderTarget->Clear(D2D1::ColorF(D2D1::ColorF::Black));
+
+        // draw here!
+
+        pRenderTarget->EndDraw();
+    }
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    switch (uMsg) {
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+    }
+    return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
